e0a081f667020ebc37bad31a48abf7f769090e19
Will Wang <will.wang6@gmail.com>
2021-02-25 21:58:43 -0500

    started combo slider

---
+++ GH_Eto/Ctrls/ExplicitSlider.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+
+namespace Synapse.Ctrls
+{
+    public class ExplicitSlider : GH_Component
+    {
+        /// <summary>
+        /// Initializes a new instance of the ExplicitSlider class.
+        /// </summary>
+        public ExplicitSlider()
+          : base("SynapseLabeledSlider", "LSlider",
+              "slider with text label",
+              "Snyapse", "Controls")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object is used to retrieve from inputs and store in outputs.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            //TODO: test combo slider
+        }
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("4880881b-8604-4fc5-90f0-e514af9e7c09"); }
+        }
+    }
+}
--- GH_Eto/Util.cs
+++ GH_Eto/Util.cs
@@ -9,7 +9,7 @@ using Grasshopper.Kernel.Data;
 
 namespace Synapse
 {
-    public class Util
+    internal class Util
     {
 
         public static void SetProp(object subject, string pname, object pval)
@@ -22,6 +22,8 @@ namespace Synapse
         {
             if (goo is GH_Point p)
                 return new Point((int)p.Value.X, (int)p.Value.Y);
+            else if (goo is GH_Vector v)
+                return new Point((int)v.Value.X, (int)v.Value.Y);
             else if (goo is GH_String s)
                 return s.Value;
             else if (goo is GH_Number n)
@@ -32,8 +34,51 @@ namespace Synapse
                 return Color.FromArgb(clr.Value.ToArgb());
             else if (goo is GH_Boolean b)
                 return b.Value;
+            else if (goo is GH_Rectangle rec)
+                return new Size((int)rec.Value.X.Length, (int)rec.Value.Y.Length);
             else
                 return goo;
+        }
+    }
+
+    internal class ComboSlider : StackLayout
+    {
+        public Slider slider;
+        public Label label;
+
+        public ComboSlider()
+        {
+            slider = new Slider()
+            {
+                MinValue = 1,
+                MaxValue = 10,
+                Value = 5,
+            };
+            label = new Label() { Text = slider.Value.ToString(), };
+            slider.ValueChanged += OnValChange;
+        }
+        public ComboSlider(int min, int max)
+        {
+            slider = new Slider()
+            {
+                MinValue = min,
+                MaxValue = max,
+                Value = (min + max) / 2,
+            };
+            label = new Label() { Text = slider.Value.ToString(), };
+            slider.ValueChanged += OnValChange;
+        }
+
+        public void Init()
+        {
+            Orientation = Orientation.Horizontal;
+            Items.Add(slider);
+            Items.Add(label);
+        }
+
+        protected void OnValChange(object s, EventArgs e)
+        {
+            label.Text = slider.Value.ToString();
         }
     }
 }
