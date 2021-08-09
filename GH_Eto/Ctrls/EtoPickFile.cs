be8eac387885a590a00671cf4f9060d98e04a7dc
Will Wang <wwang@edgnyc.com>
2021-08-09 10:46:12 -0400

    started file picker

---
+++ GH_Eto/Ctrls/EtoPickFile.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Eto.Forms;
+using Grasshopper.Kernel;
+using Grasshopper.Kernel.Types;
+
+namespace Synapse
+{
+    public class EtoPickFile : GH_Component
+    {
+        /// <summary>
+        /// Initializes a new instance of the EtoPickFile class.
+        /// </summary>
+        public EtoPickFile()
+          : base("SnpFilePicker", "SFile",
+              "pick file on hard drive",
+              "Synapse", "Controls")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddTextParameter("Property", "P", "property to set", GH_ParamAccess.list);
+            pManager[0].DataMapping = GH_DataMapping.Flatten;
+            pManager[0].Optional = true;
+            pManager.AddGenericParameter("Property Value", "V", "values for the property", GH_ParamAccess.list);
+            pManager[1].DataMapping = GH_DataMapping.Flatten;
+            pManager[1].Optional = true;
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddTextParameter("All Properties", "A", "list of all accessible properties", GH_ParamAccess.list);
+            pManager.AddGenericParameter("Control", "C", "control to go into a container or the listener", GH_ParamAccess.item);
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object is used to retrieve from inputs and store in outputs.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            List<string> props = new List<string>();
+            List<GH_ObjectWrapper> vals = new List<GH_ObjectWrapper>();
+            DA.GetDataList(0, props);
+            DA.GetDataList(1, vals);
+
+            FilePicker picker = new FilePicker();
+
+            for (int i = 0; i < props.Count; i++)
+            {
+                string n = props[i];
+                object val;
+                try { val = vals[i].Value; }
+                catch (ArgumentOutOfRangeException)
+                {
+                    AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "P, V should correspond each other");
+                    return;
+                }
+
+                if (n.ToLower() == "filetype" || n.ToLower()=="extension" || n.ToLower() == "currentfilter")
+                {
+                    if (val is GH_String gs)
+                        try
+                        {
+                            string[] n_e = gs.Value.Split('|');
+                            string[] exts = n_e[1].Split(',');
+                            picker.CurrentFilter = new FileFilter(n_e[0], exts);
+                        }
+                        catch (Exception ex)
+                        {
+                            AddRuntimeMessage(GH_RuntimeMessageLevel.Warning, " string cannot be interpreted");
+                            AddRuntimeMessage(GH_RuntimeMessageLevel.Warning, ex.Message);
+                        }
+                    else
+                        try{Util.SetProp(picker, "CurrentFilter", val);}
+                        catch (Exception ex){AddRuntimeMessage(GH_RuntimeMessageLevel.Warning, ex.Message);}
+                }
+                else if (n.ToLower() == "filterindex" || n.ToLower() == "currentfilterindex")
+                {
+                    if (val is GH_Integer gi)
+                        try { picker.CurrentFilterIndex = gi.Value; }
+                        catch { AddRuntimeMessage(GH_RuntimeMessageLevel.Warning, " filter index error"); }
+                    else if (val is GH_Number gn)
+                        try { picker.CurrentFilterIndex = (int)Math.Round(gn.Value, 0); }
+                        catch { AddRuntimeMessage(GH_RuntimeMessageLevel.Warning, " filter index error"); }
+                    else if (val is GH_Interval gitvl)
+                        try { picker.CurrentFilterIndex = (int)Math.Round(gitvl.Value.Length, 0); }
+                        catch { AddRuntimeMessage(GH_RuntimeMessageLevel.Warning, " filter index error"); }
+                    else
+                        try { Util.SetProp(picker, "CurrentFilterIndex", val); }
+                        catch { AddRuntimeMessage(GH_RuntimeMessageLevel.Warning, " filter index error"); }
+                }
+                else if (n.ToLower() == "filters" | n.ToLower() == "extensions" | n.ToLower() == "allfiletypes")
+                {
+                    if (val is GH_String gs)
+                        try
+                        {
+                            string[] lines = gs.Value.Split('\n');
+                            for (int j = 0; j < lines.Length; j++)
+                            {
+                                string[] n_e = lines[i].Split('|');
+                                string[] exts = n_e[1].Split(',');
+                                picker.Filters.Add(new FileFilter(n_e[0], exts));
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            AddRuntimeMessage(GH_RuntimeMessageLevel.Warning, " string cannot be interpreted");
+                            AddRuntimeMessage(GH_RuntimeMessageLevel.Warning, ex.Message);
+                        }
+                    else
+                        try { Util.SetProp(picker, "Filters", val); }
+                        catch (Exception ex) { AddRuntimeMessage(GH_RuntimeMessageLevel.Warning, ex.Message); }
+                }
+            }
+
+
+            PropertyInfo[] allprops = picker.GetType().GetProperties();
+            List<string> printouts = new List<string>();
+            foreach (PropertyInfo prop in allprops)
+                if (prop.CanWrite)
+                    printouts.Add(prop.Name + ": " + prop.PropertyType.ToString());
+            DA.SetDataList(0, printouts);
+        }
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("7183ae55-a6ba-4f17-b624-aaa152b9e9c0"); }
+        }
+    }
+}
---
+++ GH_Eto/Params/EtoFileFilter.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Grasshopper.Kernel;
+using Grasshopper.Kernel.Types;
+using Grasshopper.Kernel.Data;
+using Eto.Forms;
+
+namespace Synapse
+{
+    public class EtoFileFilter : GH_Component
+    {
+        /// <summary>
+        /// Initializes a new instance of the EtoFileFilter class.
+        /// </summary>
+        public EtoFileFilter()
+          : base("SnpFileType", "SExt",
+              "file types that can be recognized by the file picker",
+              "Synapse", "Parameters")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddTextParameter("Type Name", "N", "name of file types", GH_ParamAccess.list, "Plain Text");
+            pManager[0].DataMapping = GH_DataMapping.Flatten;
+            pManager[0].Optional = true;
+            pManager.AddTextParameter("Extensions", "E", "file extensions", GH_ParamAccess.tree, ".txt");
+            pManager[1].Optional = true;
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddGenericParameter("FileFilter", "F", "file type filters for the Synapse file picker", GH_ParamAccess.item);
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object is used to retrieve from inputs and store in outputs.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            GH_Structure<GH_String> exts = new GH_Structure<GH_String>();
+            List<string> names = new List<string>();
+            DA.GetDataList(0, names);
+            DA.GetDataTree(1, out exts);
+
+            if (names.Count != exts.Branches.Count)
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "an item in the list of names should match a branch of the extensions");
+                return;
+            }
+            else if (names.Count ==1 && exts.Branches.Count == 1)
+            {
+                DA.SetData(0, new FileFilter(names[0], exts.Branches[0].Select(t => t.Value).ToArray()));
+            }
+
+            FileFilter[] filters = new FileFilter[names.Count];
+            for (int i = 0; i < filters.Length; i++)
+            {
+                GH_String[] allexts = exts.Branches[0].ToArray();
+                IEnumerable<string> extstrs = allexts.Select(t => t.Value);
+                filters.SetValue(new FileFilter(names[i], extstrs.ToArray()), i);
+            }
+            DA.SetData(0, new GH_ObjectWrapper(filters));
+        }
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("14edf74c-acca-4aac-a26f-427e2f3521ea"); }
+        }
+    }
+}
