1575378c291fe0313b6cc4d26a3456943aefe9fa
Will Wang <wwang@edgnyc.com>
2021-08-13 17:34:41 -0400

    started color picker

---
+++ GH_Eto/Ctrls/EtoColorPicker.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Grasshopper.Kernel;
+using Eto.Forms;
+
+namespace Synapse
+{
+    public class EtoColorPicker : GH_Component
+    {
+        /// <summary>
+        /// Initializes a new instance of the EtoColorPicker class.
+        /// </summary>
+        public EtoColorPicker()
+          : base("SnpColorPicker", "SPickClr",
+              "Synapse color picker control",
+              "Synapse", "Controls")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddTextParameter("Property", "P", "property to set", GH_ParamAccess.list);
+            pManager[0].DataMapping = GH_DataMapping.Flatten;
+            pManager[0].Optional = true;
+            pManager.AddGenericParameter("Property Value", "V", "values for the property", GH_ParamAccess.list);
+            pManager[1].DataMapping = GH_DataMapping.Flatten;
+            pManager[1].Optional = true;
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddTextParameter("All Properties", "A", "list of all accessible properties", GH_ParamAccess.list);
+            pManager.AddGenericParameter("Control", "C", "control to go into a container or the listener", GH_ParamAccess.item);
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object is used to retrieve from inputs and store in outputs.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+        }
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                //You can add image files to your project resources and access them like this:
+                // return Resources.IconForThisComponent;
+                return null;
+            }
+        }
+
+        public override GH_Exposure Exposure
+        {
+            get { return GH_Exposure.secondary| GH_Exposure.obscure; }
+        }
+
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("ed5bab22-6431-4733-95ce-3bd840b9b563"); }
+        }
+    }
+}
